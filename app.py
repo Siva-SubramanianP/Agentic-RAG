77b7377a55b3533adca55031953f9748fde6c7a2
Siva Subramanian P <132920531+Siva-SubramanianP@users.noreply.github.com>
2026-08-18 17:02:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,58 +20,62 @@ PASSWORD = "1234"
 app.mount("/static",StaticFiles(directory="static"),name="Static")
 templates = Jinja2Templates(directory="templates")
 
+# @app.get("/",response_class=HTMLResponse)
+# async def home(request : Request):
+#     return templates.TemplateResponse(request,"login.html")
+
+
+# @app.post("/dashboard",response_class=HTMLResponse)
+# async def dashboard(request : Request):
+#     form = await request.form()
+#     if USERNAME == form["username"] and PASSWORD == form["password"]:
+#         return templates.TemplateResponse(request,"dashboard.html")
+#     else:
+#         return f"""
+#         <script>
+#             alert("Invalid Username or Password..");
+#             window.location.href = "/";
+#         </script>
+#         """
+
+# @app.get("/dashboard",response_class=HTMLResponse)
+# async def get_dashboard(request : Request):
+#     return templates.TemplateResponse(request,"dashboard.html")
+
+# @app.post("/upload")
+# async def upload_doc(documents: List[UploadFile] = File(...)):
+
+#     message, uploaded_files, failureCount = await vectorDbStoreFunc(documents)
+#     return {
+#         "message": message,
+#         "uploaded": len(uploaded_files),
+#         "failed": failureCount,
+#         "files": uploaded_files
+#     }
+
+# @app.post("/query")
+# async def query_ans(request : ChatQuestions):
+#     question = request.question
+#     answer = await retrievefromLLM(question)
+#     return{
+#         "answer" : answer
+#     }
+
+# @app.api_route("/chat",methods=["GET","POST"] ,response_class=HTMLResponse)
+# async def chatBot(request : Request):
+#     return templates.TemplateResponse(request,"chat.html")
+
+# @app.post("/botchat")
+# async def botchat(request : ChatQuestions):
+#     question = request.question
+#     answer = await chatbot(question)
+#     return{
+#         "answer" : answer
+#     }
+
 @app.get("/",response_class=HTMLResponse)
-async def home(request : Request):
-    return templates.TemplateResponse(request,"login.html")
-
-
-@app.post("/dashboard",response_class=HTMLResponse)
-async def dashboard(request : Request):
-    form = await request.form()
-    if USERNAME == form["username"] and PASSWORD == form["password"]:
-        return templates.TemplateResponse(request,"dashboard.html")
-    else:
-        return f"""
-        <script>
-            alert("Invalid Username or Password..");
-            window.location.href = "/";
-        </script>
-        """
-
-@app.get("/dashboard",response_class=HTMLResponse)
-async def get_dashboard(request : Request):
-    return templates.TemplateResponse(request,"dashboard.html")
-
-@app.post("/upload")
-async def upload_doc(documents: List[UploadFile] = File(...)):
-
-    message, uploaded_files, failureCount = await vectorDbStoreFunc(documents)
-    return {
-        "message": message,
-        "uploaded": len(uploaded_files),
-        "failed": failureCount,
-        "files": uploaded_files
-    }
-
-@app.post("/query")
-async def query_ans(request : ChatQuestions):
-    question = request.question
-    answer = await retrievefromLLM(question)
-    return{
-        "answer" : answer
-    }
-
-@app.api_route("/chat",methods=["GET","POST"] ,response_class=HTMLResponse)
-async def chatBot(request : Request):
-    return templates.TemplateResponse(request,"chat.html")
-
-@app.post("/botchat")
-async def botchat(request : ChatQuestions):
-    question = request.question
-    answer = await chatbot(question)
-    return{
-        "answer" : answer
-    }
+async def health():
+    return f"""<center><h2>Hello, World</h2></center>"""
 
 # @app.post("/botchat")
 # async def botchat(data:dict):
